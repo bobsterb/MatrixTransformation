fe4612b5b479d518094f6fc66d8da657287fe664
bobsterb <41156528+bobsterb@users.noreply.github.com>
2018-07-12 14:05:12 +0100

    Initial commit

---
+++ src/Prototype1/Prototype1/Form1.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ObjectA = New System.Windows.Forms.TextBox()
+        Me.ObjectB = New System.Windows.Forms.TextBox()
+        Me.ObjectC = New System.Windows.Forms.TextBox()
+        Me.ObjectD = New System.Windows.Forms.TextBox()
+        Me.ObjectE = New System.Windows.Forms.TextBox()
+        Me.ObjectF = New System.Windows.Forms.TextBox()
+        Me.ImageF = New System.Windows.Forms.TextBox()
+        Me.ImageE = New System.Windows.Forms.TextBox()
+        Me.ImageD = New System.Windows.Forms.TextBox()
+        Me.ImageC = New System.Windows.Forms.TextBox()
+        Me.ImageB = New System.Windows.Forms.TextBox()
+        Me.ImageA = New System.Windows.Forms.TextBox()
+        Me.MatrixD = New System.Windows.Forms.TextBox()
+        Me.MatrixC = New System.Windows.Forms.TextBox()
+        Me.MatrixB = New System.Windows.Forms.TextBox()
+        Me.MatrixA = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckButton = New System.Windows.Forms.Button()
+        Me.TextBox17 = New System.Windows.Forms.TextBox()
+        Me.ShowAnswerButton = New System.Windows.Forms.Button()
+        Me.AnswerMatrixD = New System.Windows.Forms.TextBox()
+        Me.AnswerMatrixC = New System.Windows.Forms.TextBox()
+        Me.AnswerMatrixB = New System.Windows.Forms.TextBox()
+        Me.AnswerMatrixA = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ObjectA
+        '
+        Me.ObjectA.Location = New System.Drawing.Point(333, 137)
+        Me.ObjectA.Multiline = True
+        Me.ObjectA.Name = "ObjectA"
+        Me.ObjectA.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectA.TabIndex = 0
+        '
+        'ObjectB
+        '
+        Me.ObjectB.Location = New System.Drawing.Point(413, 137)
+        Me.ObjectB.Multiline = True
+        Me.ObjectB.Name = "ObjectB"
+        Me.ObjectB.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectB.TabIndex = 1
+        '
+        'ObjectC
+        '
+        Me.ObjectC.Location = New System.Drawing.Point(500, 137)
+        Me.ObjectC.Multiline = True
+        Me.ObjectC.Name = "ObjectC"
+        Me.ObjectC.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectC.TabIndex = 2
+        '
+        'ObjectD
+        '
+        Me.ObjectD.Location = New System.Drawing.Point(333, 215)
+        Me.ObjectD.Multiline = True
+        Me.ObjectD.Name = "ObjectD"
+        Me.ObjectD.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectD.TabIndex = 3
+        '
+        'ObjectE
+        '
+        Me.ObjectE.Location = New System.Drawing.Point(413, 215)
+        Me.ObjectE.Multiline = True
+        Me.ObjectE.Name = "ObjectE"
+        Me.ObjectE.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectE.TabIndex = 4
+        '
+        'ObjectF
+        '
+        Me.ObjectF.Location = New System.Drawing.Point(500, 215)
+        Me.ObjectF.Multiline = True
+        Me.ObjectF.Name = "ObjectF"
+        Me.ObjectF.Size = New System.Drawing.Size(57, 54)
+        Me.ObjectF.TabIndex = 5
+        '
+        'ImageF
+        '
+        Me.ImageF.Location = New System.Drawing.Point(814, 215)
+        Me.ImageF.Multiline = True
+        Me.ImageF.Name = "ImageF"
+        Me.ImageF.Size = New System.Drawing.Size(57, 54)
+        Me.ImageF.TabIndex = 11
+        '
+        'ImageE
+        '
+        Me.ImageE.Location = New System.Drawing.Point(727, 215)
+        Me.ImageE.Multiline = True
+        Me.ImageE.Name = "ImageE"
+        Me.ImageE.Size = New System.Drawing.Size(57, 54)
+        Me.ImageE.TabIndex = 10
+        '
+        'ImageD
+        '
+        Me.ImageD.Location = New System.Drawing.Point(647, 215)
+        Me.ImageD.Multiline = True
+        Me.ImageD.Name = "ImageD"
+        Me.ImageD.Size = New System.Drawing.Size(57, 54)
+        Me.ImageD.TabIndex = 9
+        '
+        'ImageC
+        '
+        Me.ImageC.Location = New System.Drawing.Point(814, 137)
+        Me.ImageC.Multiline = True
+        Me.ImageC.Name = "ImageC"
+        Me.ImageC.Size = New System.Drawing.Size(57, 54)
+        Me.ImageC.TabIndex = 8
+        '
+        'ImageB
+        '
+        Me.ImageB.Location = New System.Drawing.Point(727, 137)
+        Me.ImageB.Multiline = True
+        Me.ImageB.Name = "ImageB"
+        Me.ImageB.Size = New System.Drawing.Size(57, 54)
+        Me.ImageB.TabIndex = 7
+        '
+        'ImageA
+        '
+        Me.ImageA.Location = New System.Drawing.Point(647, 137)
+        Me.ImageA.Multiline = True
+        Me.ImageA.Name = "ImageA"
+        Me.ImageA.Size = New System.Drawing.Size(57, 54)
+        Me.ImageA.TabIndex = 6
+        '
+        'MatrixD
+        '
+        Me.MatrixD.Location = New System.Drawing.Point(153, 215)
+        Me.MatrixD.Multiline = True
+        Me.MatrixD.Name = "MatrixD"
+        Me.MatrixD.Size = New System.Drawing.Size(57, 54)
+        Me.MatrixD.TabIndex = 15
+        '
+        'MatrixC
+        '
+        Me.MatrixC.Location = New System.Drawing.Point(73, 215)
+        Me.MatrixC.Multiline = True
+        Me.MatrixC.Name = "MatrixC"
+        Me.MatrixC.Size = New System.Drawing.Size(57, 54)
+        Me.MatrixC.TabIndex = 14
+        '
+        'MatrixB
+        '
+        Me.MatrixB.Location = New System.Drawing.Point(153, 137)
+        Me.MatrixB.Multiline = True
+        Me.MatrixB.Name = "MatrixB"
+        Me.MatrixB.Size = New System.Drawing.Size(57, 54)
+        Me.MatrixB.TabIndex = 13
+        '
+        'MatrixA
+        '
+        Me.MatrixA.Location = New System.Drawing.Point(73, 137)
+        Me.MatrixA.Multiline = True
+        Me.MatrixA.Name = "MatrixA"
+        Me.MatrixA.Size = New System.Drawing.Size(57, 54)
+        Me.MatrixA.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(285, 198)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "X"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(574, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "="
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(72, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 56)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckButton
+        '
+        Me.CheckButton.Location = New System.Drawing.Point(435, 321)
+        Me.CheckButton.Name = "CheckButton"
+        Me.CheckButton.Size = New System.Drawing.Size(137, 23)
+        Me.CheckButton.TabIndex = 20
+        Me.CheckButton.Text = "Check"
+        Me.CheckButton.UseVisualStyleBackColor = True
+        '
+        'TextBox17
+        '
+        Me.TextBox17.Location = New System.Drawing.Point(623, 321)
+        Me.TextBox17.Multiline = True
+        Me.TextBox17.Name = "TextBox17"
+        Me.TextBox17.Size = New System.Drawing.Size(99, 23)
+        Me.TextBox17.TabIndex = 21
+        '
+        'ShowAnswerButton
+        '
+        Me.ShowAnswerButton.Location = New System.Drawing.Point(435, 422)
+        Me.ShowAnswerButton.Name = "ShowAnswerButton"
+        Me.ShowAnswerButton.Size = New System.Drawing.Size(137, 23)
+        Me.ShowAnswerButton.TabIndex = 22
+        Me.ShowAnswerButton.Text = "Show Answer"
+        Me.ShowAnswerButton.UseVisualStyleBackColor = True
+        '
+        'AnswerMatrixD
+        '
+        Me.AnswerMatrixD.Location = New System.Drawing.Point(703, 453)
+        Me.AnswerMatrixD.Multiline = True
+        Me.AnswerMatrixD.Name = "AnswerMatrixD"
+        Me.AnswerMatrixD.Size = New System.Drawing.Size(57, 54)
+        Me.AnswerMatrixD.TabIndex = 26
+        '
+        'AnswerMatrixC
+        '
+        Me.AnswerMatrixC.Location = New System.Drawing.Point(623, 453)
+        Me.AnswerMatrixC.Multiline = True
+        Me.AnswerMatrixC.Name = "AnswerMatrixC"
+        Me.AnswerMatrixC.Size = New System.Drawing.Size(57, 54)
+        Me.AnswerMatrixC.TabIndex = 25
+        '
+        'AnswerMatrixB
+        '
+        Me.AnswerMatrixB.Location = New System.Drawing.Point(703, 375)
+        Me.AnswerMatrixB.Multiline = True
+        Me.AnswerMatrixB.Name = "AnswerMatrixB"
+        Me.AnswerMatrixB.Size = New System.Drawing.Size(57, 54)
+        Me.AnswerMatrixB.TabIndex = 24
+        '
+        'AnswerMatrixA
+        '
+        Me.AnswerMatrixA.Location = New System.Drawing.Point(623, 375)
+        Me.AnswerMatrixA.Multiline = True
+        Me.AnswerMatrixA.Name = "AnswerMatrixA"
+        Me.AnswerMatrixA.Size = New System.Drawing.Size(57, 54)
+        Me.AnswerMatrixA.TabIndex = 23
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1155, 529)
+        Me.Controls.Add(Me.AnswerMatrixD)
+        Me.Controls.Add(Me.AnswerMatrixC)
+        Me.Controls.Add(Me.AnswerMatrixB)
+        Me.Controls.Add(Me.AnswerMatrixA)
+        Me.Controls.Add(Me.ShowAnswerButton)
+        Me.Controls.Add(Me.TextBox17)
+        Me.Controls.Add(Me.CheckButton)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MatrixD)
+        Me.Controls.Add(Me.MatrixC)
+        Me.Controls.Add(Me.MatrixB)
+        Me.Controls.Add(Me.MatrixA)
+        Me.Controls.Add(Me.ImageF)
+        Me.Controls.Add(Me.ImageE)
+        Me.Controls.Add(Me.ImageD)
+        Me.Controls.Add(Me.ImageC)
+        Me.Controls.Add(Me.ImageB)
+        Me.Controls.Add(Me.ImageA)
+        Me.Controls.Add(Me.ObjectF)
+        Me.Controls.Add(Me.ObjectE)
+        Me.Controls.Add(Me.ObjectD)
+        Me.Controls.Add(Me.ObjectC)
+        Me.Controls.Add(Me.ObjectB)
+        Me.Controls.Add(Me.ObjectA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ObjectA As TextBox
+    Friend WithEvents ObjectB As TextBox
+    Friend WithEvents ObjectC As TextBox
+    Friend WithEvents ObjectD As TextBox
+    Friend WithEvents ObjectE As TextBox
+    Friend WithEvents ObjectF As TextBox
+    Friend WithEvents ImageF As TextBox
+    Friend WithEvents ImageE As TextBox
+    Friend WithEvents ImageD As TextBox
+    Friend WithEvents ImageC As TextBox
+    Friend WithEvents ImageB As TextBox
+    Friend WithEvents ImageA As TextBox
+    Friend WithEvents MatrixD As TextBox
+    Friend WithEvents MatrixC As TextBox
+    Friend WithEvents MatrixB As TextBox
+    Friend WithEvents MatrixA As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckButton As Button
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents ShowAnswerButton As Button
+    Friend WithEvents AnswerMatrixD As TextBox
+    Friend WithEvents AnswerMatrixC As TextBox
+    Friend WithEvents AnswerMatrixB As TextBox
+    Friend WithEvents AnswerMatrixA As TextBox
+End Class
---
+++ src/Prototype1/Prototype1/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+
+    Dim MyRandom As Random
+    Dim MatrixCheck(3) As Integer
+
+    Sub PopulateWithRandomNumbers()
+        MyRandom = New Random
+
+
+        MatrixA.Text = MyRandom.Next(0, 10)
+        MatrixB.Text = MyRandom.Next(0, 10)
+        MatrixC.Text = MyRandom.Next(0, 10)
+        MatrixD.Text = MyRandom.Next(0, 10)
+        ObjectA.Text = MyRandom.Next(0, 10)
+        ObjectB.Text = MyRandom.Next(0, 10)
+        ObjectC.Text = MyRandom.Next(0, 10)
+        ObjectD.Text = MyRandom.Next(0, 10)
+        ObjectE.Text = MyRandom.Next(0, 10)
+        ObjectF.Text = MyRandom.Next(0, 10)
+        'ImageF.Text = MyRandom.Next(0, 10)
+        'ImageE.Text = MyRandom.Next(0, 10)
+        'ImageD.Text = MyRandom.Next(0, 10)
+        'ImageC.Text = MyRandom.Next(0, 10)
+        'ImageB.Text = MyRandom.Next(0, 10)
+        'ImageA.Text = MyRandom.Next(0, 10)
+
+        CalculateImage()
+        ClearingMatrix()
+
+    End Sub
+    Sub CalculateImage()
+        ImageA.Text = (MatrixA.Text * ObjectA.Text) + (MatrixB.Text * ObjectD.Text)
+        ImageB.Text = (MatrixA.Text * ObjectB.Text) + (MatrixB.Text * ObjectE.Text)
+        ImageC.Text = (MatrixA.Text * ObjectC.Text) + (MatrixB.Text * ObjectF.Text)
+        ImageD.Text = (MatrixC.Text * ObjectA.Text) + (MatrixD.Text * ObjectD.Text)
+        ImageE.Text = (MatrixC.Text * ObjectB.Text) + (MatrixD.Text * ObjectE.Text)
+        ImageF.Text = (MatrixC.Text * ObjectA.Text) + (MatrixD.Text * ObjectF.Text)
+    End Sub
+
+    Private Sub s(sender As Object, e As EventArgs) Handles ObjectA.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateWithRandomNumbers()
+        ShowAnswerButton.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PopulateWithRandomNumbers()
+    End Sub
+    Sub ClearingMatrix()
+
+        MatrixCheck(0) = MatrixA.Text
+        MatrixCheck(1) = MatrixB.Text
+        MatrixCheck(2) = MatrixC.Text
+        MatrixCheck(3) = MatrixD.Text
+
+        MatrixA.Text = String.Empty
+        MatrixB.Text = String.Empty
+        MatrixC.Text = String.Empty
+        MatrixD.Text = String.Empty
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles CheckButton.Click
+        ShowAnswerButton.Show()
+
+        Dim correct As Boolean = True
+
+
+        If MatrixA.Text = MatrixCheck(0) And MatrixB.Text = MatrixCheck(1) And MatrixC.Text = MatrixCheck(2) And MatrixD.Text = MatrixCheck(3) Then
+            correct = True
+        Else
+            correct = False
+        End If
+
+        If correct = True Then
+            TextBox17.Text = "Correct"
+        Else
+            TextBox17.Text = "Incorrect"
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles ShowAnswerButton.Click
+        AnswerMatrixA.Text = MatrixCheck(0)
+        AnswerMatrixB.Text = MatrixCheck(1)
+        AnswerMatrixC.Text = MatrixCheck(2)
+        AnswerMatrixD.Text = MatrixCheck(3)
+    End Sub
+
+
+
+
+
+    'Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    '    Dim correct As Boolean = True
+
+    'If (MatrixA.Text * ObjectA.Text) + (MatrixB.Text * ObjectD.Text) = ImageA.Text And (MatrixA.Text * ObjectA.Text) + (MatrixB.Text * ObjectD.Text) = ImageB.Text And (MatrixA.Text * ObjectB.Text) + (MatrixB.Text * ObjectE.Text) = ImageB.Text And (MatrixA.Text * ObjectC.Text) + (MatrixB.Text * ObjectF.Text) = ImageC.Text And (MatrixC.Text * ObjectA.Text) + (MatrixD.Text * ObjectD.Text) = ImageD.Text And (MatrixC.Text * ObjectB.Text) + (MatrixD.Text * ObjectE.Text) = ImageE.Text And (MatrixC.Text * ObjectC.Text) + (MatrixD.Text * ObjectF.Text) = ImageF.Text Then
+    '       correct = True
+    'Else correct = False
+    '
+    'End If
+
+    '  If correct = True Then
+    '       TextBox17.Text = "Correct"
+    'Else
+    '       TextBox17.Text = "Incorrect"
+    'End If
+
+    'End Sub
+
+
+End Class
